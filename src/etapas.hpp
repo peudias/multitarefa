39bbc038e673eeaa921902bbf6be1de3b1dd64c8
p8 <phpdias@outlook.com>
2022-10-14 17:53:21 -0300

    Update etapas.hpp

--- src/etapas.hpp
+++ src/etapas.hpp
@@ -13,15 +13,15 @@ using namespace std;
 
 void menu();
 
-void tokenizar(string text, unordered_map<string, vector<int>> *mapa, int linha);
+void tokenizar(string text, unordered_map<string, vector<int>> *mapa, int linha, unordered_map<string, vector<int>> *classe);
 
 void tokenizar_etapaII(string text, unordered_map<int, vector<string>> *mapa, int linha);
 
-void etapaI(unordered_map<string, vector<int>> *mapa_d);
+void etapaI(unordered_map<string, vector<int>> *mapa_d, unordered_map<string, vector<int>> *classe);
 
-void etapaII(unordered_map<string, vector<int>> *mapa_d);
+void etapaII(unordered_map<string, vector<int>> *mapa_d, unordered_map<string, vector<int>> *classe);
 
-void etapaIII(unordered_map<string, vector<int>> *mapa_d, unordered_map<int, vector<string>> *mapa_t_reduzido_permutacao);
+void etapaIII(unordered_map<string, vector<int>> *mapa_d, unordered_map<int, vector<string>> *mapa_t_reduzido_permutacao, unordered_map<string, vector<int>> *classe);
 
 void permutacao(unordered_map<int, vector<string>> *mapa_t_reduzido_permutacao,vector <string> vaux, int linha);
 
